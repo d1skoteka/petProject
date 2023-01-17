df40c6e82dc1ba03653965ed68bd9d1865651196
xxMolnia <114886266+xxMolnia@users.noreply.github.com>
2023-01-18 00:16:03 +0100

    Include bootstarp and jquery

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <html>
 <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- include jquery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script> 
+    <!-- include bootstarp -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    
 </head>
 <body>
 
